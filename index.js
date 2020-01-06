8e762f15cbe8b9053c410f7284143b0a515b9417
Nguyen Ngoc Tu <ngoctuct@gmail.com>
2020-01-06 21:39:48 +0700

    update user and pass

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (process.env.PROXY_USE) {
       agent: new HttpsProxyAgent({
         host: process.env.PROXY_HOST,
         port: process.env.PROXY_PORT,
-        auth: "cardct:abcd@1234",
+        auth: process.env.user + ":" process.env.pass,
         ca: [fs.readFileSync("BIDVCA.crt")],
       }),
     },
